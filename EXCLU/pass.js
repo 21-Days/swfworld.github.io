73e9b92cbb6203ee59e05fc4589fe63c7be0f39a
ROADKILLSANTA <ericnie7@gmail.com>
2018-01-26 23:48:36 -0500

    Update pass.js

--- EXCLU/pass.js
+++ EXCLU/pass.js
@@ -1,3 +1,31 @@
+function setCookie(cname, cvalue, exhours) {
+		var d = new Date();
+		d.setTime(d.getTime() + (exhours * 60 * 60 * 1000));
+		var expires = "expires="+d.toUTCString();
+		document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
+	}
+	function getCookie(cname) {
+		var name = cname + "=";
+		var ca = document.cookie.split(';');
+		for(var i = 0; i < ca.length; i++) {
+			var c = ca[i];
+			while (c.charAt(0) == ' ') {
+				c = c.substring(1);
+			}
+			if (c.indexOf(name) == 0) {
+				return c.substring(name.length, c.length);
+			}
+		}
+		return "";
+	}
+	function checkCookie(cookie) {
+		var user = getCookie(cookie);
+		if (loggedIn == "True") {
+			alert("Welcome again!");
+		} else {
+			window.location.href = "/EXCLU/";
+		}
+	}
 function passWord() {
 	function stringToBinary(str, spaceSeparatedOctets) {
 		function zeroPad(num) {
@@ -15,6 +43,7 @@ function passWord() {
 		if (pass1.toLowerCase() == stringToBinary(FileReader.readAsText("/EXCLU/passwd.txt");)).toLowerCase() {
 			alert('You Got it Right!');
 			window.open('protectpage.html');
+			setcookie("loggedIn", "True", 1);
 			break;
 		} 
 		testV+=1;
