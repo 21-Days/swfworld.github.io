112cedd8e52384f4c4d01b5ed7d42410ac96798b
ROADKILLSANTA <ericnie7@gmail.com>
2018-01-26 23:54:48 -0500

    Update pass.js

--- EXCLU/pass.js
+++ EXCLU/pass.js
@@ -4,53 +4,51 @@ function setCookie(cname, cvalue, exhours) {
 		var expires = "expires="+d.toUTCString();
 		document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
 	}
-	function getCookie(cname) {
-		var name = cname + "=";
-		var ca = document.cookie.split(';');
-		for(var i = 0; i < ca.length; i++) {
-			var c = ca[i];
-			while (c.charAt(0) == ' ') {
-				c = c.substring(1);
-			}
-			if (c.indexOf(name) == 0) {
-				return c.substring(name.length, c.length);
-			}
+function getCookie(cname) {
+	var name = cname + "=";
+	var ca = document.cookie.split(';');
+	for(var i = 0; i < ca.length; i++) {
+		var c = ca[i];
+		while (c.charAt(0) == ' ') {
+			c = c.substring(1);
 		}
-		return "";
-	}
-	function checkCookie(cookie) {
-		var user = getCookie(cookie);
-		if (loggedIn == "True") {
-			alert("Welcome again!");
-		} else {
-			window.location.href = "/EXCLU/";
+		if (c.indexOf(name) == 0) {
+			return c.substring(name.length, c.length);
 		}
 	}
-function passWord() {
-	function stringToBinary(str, spaceSeparatedOctets) {
-		function zeroPad(num) {
-			return "00000000".slice(String(num).length) + num;
-		}
-		return str.replace(/[\s\S]/g, function(str) {
-			str = zeroPad(str.charCodeAt().toString(2));
-			return !1 == spaceSeparatedOctets ? str : str + " "
-		});
-	};
-	var testV = 1;
-	var pass1 = prompt('Please Enter Your Password','Password:');
-	while (testV < 3) {
-		if (!pass1){history.go(-1);}
-		if (pass1.toLowerCase() == stringToBinary(FileReader.readAsText("/EXCLU/passwd.txt");)).toLowerCase() {
-			alert('You Got it Right!');
-			window.open('protectpage.html');
-			setcookie("loggedIn", "True", 1);
-			break;
-		} 
-		testV+=1;
-		var pass1 = prompt('Access Denied - Password Incorrect, Please Try Again.','Password:');
+	return "";
+}
+function checkCookie(cookie) {
+	var user = getCookie(cookie);
+	if (loggedIn == "True") {
+		alert("Welcome again!");
+	} else {
+		window.location.href = "/EXCLU/";
 	}
-	if (pass1.toLowerCase()!="password" & testV ==3) {
-		history.go(-1);
-		return " ";
+}
+function stringToBinary(str, spaceSeparatedOctets) {
+	function zeroPad(num) {
+		return "00000000".slice(String(num).length) + num;
 	}
+	return str.replace(/[\s\S]/g, function(str) {
+		str = zeroPad(str.charCodeAt().toString(2));
+		return !1 == spaceSeparatedOctets ? str : str + " "
+	});
 };
+var testV = 1;
+var pass1 = prompt('Please Enter Your Password','Password:');
+while (testV < 3) {
+	if (!pass1){history.go(-1);}
+	if (pass1.toLowerCase() == stringToBinary(FileReader.readAsText("/EXCLU/passwd.txt");)).toLowerCase() {
+		alert('You Got it Right!');
+		window.open('protectpage.html');
+		setcookie("loggedIn", "True", 1);
+		break;
+	} 
+	testV+=1;
+	var pass1 = prompt('Access Denied - Password Incorrect, Please Try Again.','Password:');
+}
+if (pass1.toLowerCase()!="password" & testV ==3) {
+	history.go(-1);
+	return " ";
+}
