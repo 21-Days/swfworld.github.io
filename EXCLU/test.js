2f9c7532c06f2dd1be681ae71314057f5dfd6807
ROADKILLSANTA <ericnie7@gmail.com>
2018-01-31 23:54:35 -0500

    Update test.js

--- EXCLU/test.js
+++ EXCLU/test.js
@@ -1,16 +1,13 @@
-var mysql      = require('mysql');
+var mysql = require('mysql');
 var connection = mysql.createConnection({
-host     : 'localhost',
-user     : '< MySQL username >',
-password : '< MySQL password >',
-database : '<your database name>'
+host     : 'sql9.freemysqlhosting.net',
+user     : 'sql9217870',
+password : 'pFPkqQSabh',
+database : 'sql9217870'
 });
 connection.connect();
-connection.query('SELECT usernames from accounts', function(err, rows, fields) {
-if (!err)
-console.log('The solution is: ', rows);
-else
-console.log('Error while performing Query.');
+connection.query('SELECT usr, from accounts', function(err, rows, fields) {
+if (!err) {console.log('The solution is: ', rows);}
+else {console.log('Error while performing Query.');}
 });
-
 connection.end();
