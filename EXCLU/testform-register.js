b001cbe967cc4e92f97d2b3c3f748c8d5b28a0c6
ROADKILLSANTA <ericnie7@gmail.com>
2018-02-02 09:05:07 -0500

    Update testform-register.js

--- EXCLU/testform-register.js
+++ EXCLU/testform-register.js
@@ -1,11 +1,14 @@
 function register(){
+	function binaryAgent(){
+	}
 	var mysql = require('mysql');
 	var con = mysql.createConnection({
 			host: "maindb.czwi63gmjfs0.us-east-1.rds.amazonaws.com",
-			user: "yourusername",
-			password: "yourpassword",
+			user: "getInfo",
+			password: binaryAgent(),
 			database: "maindb"
 	});
+	var flag=false;
 	con.connect(function(err) {
 		if (err){throw err;}
 		console.log("Connected!");
@@ -15,32 +18,52 @@ function register(){
 			con.query(sql, function(err, result){
 				if(err){throw err;}
 				console.log("got confirmation");
-			});
-			var i=0;
-			while(i<sql.length){
-				if(sql[i]==document.getElementById("registerKey").value){
-					alert("Register key correct!");
-					break;
+				var i=0;
+				while(i<result.length){
+					if(result[i]=="0" || result[i]==0){
+						alert("Register key correct!");
+						registered=true;
+						return=" ";
+					}
+					else{
+						alert("Sorry, but your register key was incorrect.");
+						document.getElementById("regkeythrow").innerHTML="Register key incorrect";
+						flag=true;
+						break;
+					}
+					i++;
 				}
-				else{
-					alert("Sorry, but your register key was incorrect.");}
-				i++;
+				if(flag=true){
+					return=" ";
+				}
+			});
+			if(flag="true"){
+				break;
 			}
 		}
-		var sql="SELECT userId FROM EXCLU_accounts"; /*NEEDS WORK HERE*/
+		if(document.getElementById("pwd").value != document.getElementById("pwdconf").value){
+			document.getElementById("pwdthrow").innerHTML="Passwords do not match!";
+			var flag=true;
+		}
+		if(flag=true){
+			return=" ";
+		}
+		var lastnum=0;
+		var sql="SELECT userId FROM EXCLU_accounts";
 		con.query(sql, function(err, result){
 			if(err){throw err;}
 			console.log("retrieved database info");
 			var isRegistered=false;
-			while(isRegistered==false){
-
-			}
+			lastnum=result(-1).pop();
 		})
-		var sql = "INSERT INTO EXCLU_accounts (user, pwd, registerKey, accountType) VALUES ("+document.getElementById('user').value+", "+document.getElementById("pwd")+", "+document.getElementById("registerKey")+", 1)";
+		var sql = "INSERT INTO EXCLU_accounts (userId, user, pwd, registerKey, accountType) VALUES ("+lastnum+", "+document.getElementById('user').value+", "+document.getElementById("pwd")+", "+document.getElementById("registerKey")+", 1)";
 		con.query(sql, function (err, result) {
 			if (err){throw err;}
 			console.log("1 record inserted");
 			alert("Success!");
 		});
 	});
+	if(flag==true){
+		return=" ";
+	}
 }
