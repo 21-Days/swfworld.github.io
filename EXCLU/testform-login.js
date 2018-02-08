c8050fd176d36135ac322d8b10cf4b6305692268
ROADKILLSANTA <ericnie7@gmail.com>
2018-02-08 12:16:08 -0500

    Update testform-login.js

--- EXCLU/testform-login.js
+++ EXCLU/testform-login.js
@@ -1,3 +1,4 @@
+var isuser=false;
 function login(){
 	function binaryAgent(){var strArray="01000101 01110010 01101001 01100011 01101110 01101001 01100101 00110001 00111001 00111001 00111000 0001010".split(" ");var text="";for(var i=0;i<strArray.length;i++){var char=parseInt(strArray[i],2).toString(10);char=String.fromCharCode(char);text+=char;}return text;}
 	var mysql = require('mysql');
@@ -8,22 +9,26 @@ function login(){
 		database : 'maindb'
 	});
 	connection.connect(function(err){
-	connection.query('SELECT user from EXCLU_accounts WHERE user='+document.getElementById("user").value, function(err, result) {
-		if (!err) {console.log('The solution is: ', rows);}
-		else {console.log('Error while performing Query.');}
-		var i=0;
-		var isuser=false;
-		while(i<result.length){
-			if(result[i]==document.getElementById("user").value){
-				var isuser=true;
-				break;
-			}
-			else{
-				var isuser=false;
-				break;
+	connection.query('SELECT user from EXCLU_accounts WHERE user='+document.getElementById("user").value+document.getElementById("pwd"), function(err, result) {
+		if(!err){
+			var i=0;
+			while(i<result.length){
+				if(result[i]==document.getElementById("user").value){
+					isuser=true;
+					break;
+				}
+				else{
+					isuser=false;
+					break;
+				}
 			}
 		}
+		else {console.log('Error while performing Query.');}
 	});
+	if(isuser==false){
+		return " ";
+	}
+	connection.query('', function(err))
 	if(){}
 	});
 	connection.end();
