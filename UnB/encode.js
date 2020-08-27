35ec64b4a026d34ba2ee56be2e27f219e205ec4a
ROADKILLSANTA <ericnie7@gmail.com>
2020-08-27 03:46:50 -0400

    Update encode.js

--- UnB/encode.js
+++ UnB/encode.js
@@ -28,42 +28,20 @@ function onload(realnum){
 		}
 	});
 	var iframe=document.getElementById("pxy");
-	var remove=iframe.contentWindow.document.getElementById("foreverproxy-top");
-	var remove2=iframe.contentWindow.document.getElementById("foreverproxy-bottom");
-	iframe.addEventListener("load", function(){
-		document.getElementById("searchbox2").value=atob.decode(iframe.src.replace(baseurl, ""));
-		remove.style.display="none";
-		remove2.style.display="none";
-	});
 }
 function google(num){
 	if (!document.getElementById("searchbox").value){
 		alert("ERROR: No input!");
 	}
 	else {
-		if(num==1){
-			var normlink = document.getElementById("searchbox").value;
-			var n2="https://www.google.com/search?q="+normlink;
-			var encodedlink = btoa(n2);
-			console.log(encodedlink);
-			var iframe=document.getElementById("pxy");
-			document.getElementById("topdiv").style.display="block";
-			document.getElementById("mainelems").style.display="none";
-			iframe.src="https://phx.4everproxy.com/direct/"+encodedlink;
-			$(iframe).contents().find("foreverproxy-top").remove();
-			iframe.style.display="block";
-		}
-		else if(num==2){
-			var normlink = document.getElementById("searchbox").value;
-			var n2="https://www.google.com/search?q="+normlink;
-			var encodedlink = btoa(n2);
-			console.log(encodedlink);
-			var iframe=document.getElementById("pxy");
-			document.getElementById("topdiv").style.display="block";
-			document.getElementById("mainelems").style.display="none";
-			iframe.src="http://unbpxy.000webhostapp.com/phpxy/index.php?q="+encodedlink;
-			iframe.style.display="block";
-		}
+		var normlink = document.getElementById("searchbox").value;
+		var n2="https://www.google.com/search?q="+normlink;
+		var iframe=document.getElementById("pxy");
+		document.getElementById("topdiv").style.display="block";
+		document.getElementById("mainelems").style.display="none";
+		iframe.src="https://node.roadkillsanta.repl.co/proxy/"+normlink;
+		$(iframe).contents().find("foreverproxy-top").remove();
+		iframe.style.display="block";
 	}
 }
 function go(num){
@@ -71,26 +49,14 @@ function go(num){
 		alert("ERROR: No input!");
 	}
 	else {
-		if(num==1){
-			var normlink = document.getElementById("searchbox").value;
-			var encodedlink = btoa(normlink);
-			console.log(encodedlink);
-			var iframe=document.getElementById("pxy");
-			document.getElementById("topdiv").style.display="block";
-			document.getElementById("mainelems").style.display="none";
-			iframe.src="http://phx.4everproxy.com/direct/"+encodedlink;
-			iframe.style.display="block";
-		}
-		else if(num==2){
-			var normlink = document.getElementById("searchbox").value;
-			var encodedlink = btoa(normlink);
-			console.log(encodedlink);
-			var iframe=document.getElementById("pxy");
-			document.getElementById("topdiv").style.display="block";
-			document.getElementById("mainelems").style.display="none";
-			iframe.src="http://unbpxy.000webhostapp.com/phpxy/index.php?q="+encodedlink;
-			iframe.style.display="block";
-		}
+		var normlink = document.getElementById("searchbox").value;
+		var encodedlink = btoa(normlink);
+		console.log(encodedlink);
+		var iframe=document.getElementById("pxy");
+		document.getElementById("topdiv").style.display="block";
+		document.getElementById("mainelems").style.display="none";
+		iframe.src="https://node.roadkillsanta.repl.co/proxy/"+encodedlink;
+		iframe.style.display="block";
 	}
 }
 function tinysearch(num){
